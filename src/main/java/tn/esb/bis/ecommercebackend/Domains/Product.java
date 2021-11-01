2381a825b9c0cafa2e1ddf41215bfadca9b0af9b
BigBoss2011 <chaoukibayoudhi@gmail.com>
2021-11-01 15:26:52 +0100

    Update Product and Provider

--- src/main/java/tn/esb/bis/ecommercebackend/Domains/Product.java
+++ src/main/java/tn/esb/bis/ecommercebackend/Domains/Product.java
@@ -4,13 +4,19 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Arrays;
 
-@Getter
-@Setter
+
 @NoArgsConstructor
-@RequiredArgsConstructor
-@ToString
-@EqualsAndHashCode
+@Data
+//@ToString //==>redifinition de la methode String toString() de la classe Object
+//retourne les valeurs de tous les attributs
+//@ToString(exclude = {"id","photo"})//retourne les valeurs de tous les champs sauf id et photo
+@ToString(exclude = "photo")
+//@EqualsAndHashCode //redefinit les deux méthodes boolean equals() et
+//hashCode() déjà definis dans la classe Object
+//cette option compare les objets en se basant sur tous les champs
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity //La classe Product sera transformée en table par l'ORM
 //ORM: Object Relationnal Mapper
 public class Product {
@@ -18,15 +24,19 @@ public class Product {
     //@GeneratedValue //une valeur de clé auto-incrimentée (par defaut identité)
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
-    @NonNull
+    @NonNull  //L'attribut "label" est obligatoire
+    @EqualsAndHashCode.Include //Label va être prise en consideration pour comparer deux produits
     private String label;
     @NonNull
     private double price;
+    @NonNull
+    @EqualsAndHashCode.Include
     private LocalDate endConsumeDate;
+    @NonNull
+    @EqualsAndHashCode.Include
     private LocalDate fabricationDate;
     private String description;
     @Lob
     private byte[] photo;
 
-    
 }
--- src/main/java/tn/esb/bis/ecommercebackend/Domains/Provider.java
+++ src/main/java/tn/esb/bis/ecommercebackend/Domains/Provider.java
@@ -1,4 +1,20 @@
 package tn.esb.bis.ecommercebackend.Domains;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Provider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String telephone;
+    private String siteUrl;
 }
